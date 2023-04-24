fcb716a7338854cdfa192311446b2dc7f5544cf0
MohamedHani <mohamedhanikhafaga@gmail.com>
2023-04-24 05:24:10 +0200

    inde

--- CRUD.js
+++ CRUD.js
@@ -6,6 +6,7 @@ let discount = document.getElementById("discount")
 let totalSalary = document.getElementById("totalSalary")
 let count = document.getElementById("count")
 let category = document.getElementById("category")
+let create = document.getElementById("create")
 let search = document.getElementById("search")
 let searchTittle = document.getElementById("search-tittle")
 let searchCategory = document.getElementById("search-category")
@@ -19,4 +20,26 @@ let total = function () {
         totalSalary.innerHTML = "";
         totalSalary.style.backgroundColor = "rgb(82, 4, 103)";
     }
+}
+
+let createPro;
+if (localStorage.product != null) {
+    createPro = JSON.parse(localStorage.product)
+} else {
+    createPro = [];
+}
+
+create.onclick = function () {
+    let obj = {
+        tittle: tittle.value,
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discount: discount.value,
+        totalSalary: totalSalary.innerHTML,
+        category: category.value
+    }
+    createPro.push(obj);
+    localStorage.setItem("Product", JSON.stringify(createPro))
+    console.log(createPro)
 }
